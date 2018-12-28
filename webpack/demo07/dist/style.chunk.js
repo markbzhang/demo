a1505fd38c247c550671c62557d3df20b8497b32
markbzhang <421902143@qq.com>
2018-12-28 21:25:37 +0800

    webpack4SCSS提取和懒加载

---
+++ webpack/demo07/dist/style.chunk.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],[,,function(o,n,r){(o.exports=r(1)(!1)).push([o.i,"html {\n  background-color: green !important; }\n",""])},function(o,n,r){var t=r(2);"string"==typeof t&&(t=[[o.i,t,""]]);var i={hmr:!0,transform:void 0,insertInto:void 0};r(0)(t,i);t.locals&&(o.exports=t.locals)}]]);
